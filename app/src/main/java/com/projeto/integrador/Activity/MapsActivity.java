884afaa8514083d02c1e89fd477aaa8997135d07
BoraRodrigo <rodrigobora93@gmail.com>
2018-09-03 15:00:48 -0300

    Menu Sair na Classe Maps.Ver Anotações

--- app/src/main/java/com/projeto/integrador/Activity/MapsActivity.java
+++ app/src/main/java/com/projeto/integrador/Activity/MapsActivity.java
@@ -7,7 +7,11 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,22 +20,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.projeto.integrador.Configuracoes.ConfiguracaoFirebase;
 import com.projeto.integrador.Configuracoes.PermissoesMaps;
 import com.projeto.integrador.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+                                  /// Esta Classe estendia de "FragmentActivity" ai para Adiconar uma Action bar mudei para AppCompa
 
     private GoogleMap mMap;
 
-
-
+    private FirebaseAuth autenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-
-
+        autenticacao= ConfiguracaoFirebase.getAutenticacao();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -87,5 +92,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return false;
             }
         });
+
+
+    }
+
+    //Configurando menu de sair adiconado evento a ele este menu é criado na pasta menu e instaciaodo aqui
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_maps,menu);
+       return true;
+    }
+
+   //define o que ocorre se sair for selecionado
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case  R.id.menuSair:
+               autenticacao.signOut();
+               finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
